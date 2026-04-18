43ad3b14499362337992032ab2197db55c3e4063
Khushi256 <khushim1109@gmail.com>
2026-04-18 18:27:32 +0530

    feat: deploy application to Vercel with output functions and static assets

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-LwkLA-fU.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/khush/OneDrive/Desktop/gitsmap/github-visualizer/src/routes/__root.tsx", children: ["/", "/$username"], assets: void 0, preloads: ["/assets/index-TuYX0xd0.js"] }, "/": { filePath: "C:/Users/khush/OneDrive/Desktop/gitsmap/github-visualizer/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-A2I_ys40.js"] }, "/$username": { filePath: "C:/Users/khush/OneDrive/Desktop/gitsmap/github-visualizer/src/routes/$username.tsx", children: void 0, assets: void 0, preloads: ["/assets/_username-_iSBN62d.js"] } }, clientEntry: "/assets/index-TuYX0xd0.js" });
+export {
+  tsrStartManifest
+};
